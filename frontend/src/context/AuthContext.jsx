0e6ba48c69f5df8af35e756eb062c627d6dc5f43
chahatjain2527 <chahatjain2527@gmail.com>
2026-04-26 22:59:04 +0530

    added manifest for PWA and change favicon and app logo

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState, useContext, createContext } from "react";
+import jwtDecode from "jwt-decode";
+
+const AuthContext = createContext();
+
+export const AuthProvide = ({child}) =>{
+    const [user, setUser] = useState(null);
+
+    useEffect(()=>{
+        const token = localStorage.getItem("token");
+        if(token){
+            try {
+                const decode = jwtDecode(token);
+                setUser(decode);
+            } catch (error) {
+                console.log("Invalid Token");
+                setUser(null);
+                logout();
+            }
+        }
+    },[]);
+
+    const login = (token)=>{
+        localStorage.setItem("token",token);
+        const decoded = jwtDecode(token);
+        setUser(decoded);
+    }
+
+    const logout = ()=>{
+        localStorage.removeItem("token");
+        setUser(null);
+        window.location.href = "/login";
+    }
+    return (
+        <AuthContext.Provider
+          value={{
+            user,
+            isAuthenticated: !!user,
+            login,
+            logout,
+          }}
+        >
+          {children}
+        </AuthContext.Provider>
+      );
+}
+
+export const useAuth = () =>{
+    useContext(AuthContext);
+}
