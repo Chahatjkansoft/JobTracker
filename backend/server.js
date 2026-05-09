5af16c47bcf04f915b6d82885df2fa9d03b322f9
chahatjain2527 <chahatjain2527@gmail.com>
2026-05-09 22:04:26 +0530

    corse origin code change

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ const applicationRoutes = require("./routes/applicationRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
